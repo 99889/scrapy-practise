4084034409b59159e82cc897cb7dd30c70afd44a
Inshaal Moin <moininshaal7@gmail.com>
2024-07-02 11:05:44 +0530

    implemented mysql

--- quote/pipelines.py
+++ quote/pipelines.py
@@ -1,25 +1,60 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+# # Define your item pipelines here
+# #
+# # Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-# useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
-import pymongo
+# # useful for handling different item types with a single interface
+# from itemadapter import ItemAdapter
+# import pymongo
 
-class QuotePipeline:
+# class QuotePipeline:
 
-    def __init__(self):
-        self.conn = pymongo.MongoClient(
-            'localhost',
-            27017
-        )
-        db = self.conn['myquotes']
-        self.collection = db['quotes_tb']
+#     def __init__(self):
+#         self.conn = pymongo.MongoClient(
+#             'localhost',
+#             27017
+#         )
+#         db = self.conn['myquotes']
+#         self.collection = db['quotes_tb']
 
-    def process_item(self, item, spider):
-        self.collection.insert_one(dict(ItemAdapter(item).asdict()))
-        return item
+#     def process_item(self, item, spider):
+#         self.collection.insert_one(dict(ItemAdapter(item).asdict()))
+#         return item
 
     
+import mysql.connector
+from itemadapter import ItemAdapter
+
+class QuotePipeline:
+    def __init__(self):
+        self.conn = mysql.connector.connect(
+            host='localhost',
+            user='root',        
+            password='inshaalsql@123',    
+            database='myquotes'     
+        )
+        self.cursor = self.conn.cursor()
+        self.create_table()
+
+    def create_table(self):
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS quotes_tb (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                title TEXT,
+                author VARCHAR(255),
+                tag TEXT
+            )
+        """)
+
+    def process_item(self, item, spider):
+        data = ItemAdapter(item).asdict()
+        self.cursor.execute("""
+            INSERT INTO quotes_tb (title, author, tag) VALUES (%s, %s, %s)
+        """, (data['title'], data['author'], ','.join(data['tag'])))
+        self.conn.commit()
+        return item
+
+    def close_spider(self, spider):
+        self.cursor.close()
+        self.conn.close()
